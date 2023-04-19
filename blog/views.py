b88b922a128e16e81a4ab68b7955c7ca83b67a29
AlexPerez7 <alexm.perez.argomedo@gmail.com>
2023-04-19 18:34:13 -0400

    post categorias

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Post, Categoria
+
 
 # Create your views here.
+
+def blog(request):
+    posts = Post.objects.all() #trae todos los posts
+    return render(request, "blog/blog.html", {'posts':posts})
+
+def categoria(request, categoria_id):
+    categoria = Categoria.objects.get(id=categoria_id)
+    posts = Post.objects.filter(categorias=categoria)
+    return render(request, "blog/categorias.html", {'categoria':categoria, 'posts':posts})
