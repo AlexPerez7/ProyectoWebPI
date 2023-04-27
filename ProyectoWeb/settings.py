353a6db2b04dcbba6901c1dee419117987ef5f6c
AlexPerez7 <alexm.perez.argomedo@gmail.com>
2023-04-26 22:45:51 -0400

    tienda, carro

--- ProyectoWeb/settings.py
+++ ProyectoWeb/settings.py
@@ -38,9 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'ProyectoWebApp',
     'Servicios',
     'blog',
+    'contacto',
+    'tienda',
+    'carro',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +70,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'carro.context_processor.importe_total_carro',
             ],
         },
     },
@@ -129,3 +134,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configuración de envío de correo
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER ="a.perez.argomedo@gmail.com"
+EMAIL_HOST_PASSWORD = "ArgoxSeven7"
